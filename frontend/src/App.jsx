b4492372d3a351ff0abaebe150e593afb15a3775
ayan-rick <ayanchatterjeemail03@gmail.com>
2026-09-26 19:41:04 +0530

    Add admin students page route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,9 @@ import PlacementDriveDetails from "./pages/PlacementDriveDetails";
 import MyApplications from "./pages/MyApplications";
 import MyProfile from "./pages/MyProfile";
 import AdminDashboard from "./pages/AdminDashboard";
+import AdminStudents from "./pages/AdminStudents";
 import AdminRoute from "./components/AdminRoute";
+
 
 import ProtectedRoute from "./components/ProtectedRoute";
 
@@ -72,12 +74,19 @@ function App() {
                     />
 
                     <Route element={<AdminRoute />}>
+
                         <Route
                             path="/admin/dashboard"
                             element={<AdminDashboard />}
                         />
+
+                        <Route
+                            path="/admin/students"
+                            element={<AdminStudents />}
+                        />
+
                     </Route>
-                    
+
                     <Route
                         path="/"
                         element={
